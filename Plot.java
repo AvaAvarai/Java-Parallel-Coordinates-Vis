189d4643e1334a03b4393f620f69c16a93bc082a
AvaAvarai <alice.williams.tech@gmail.com>
2023-06-25 10:54:23 -0700

    Axis normalization fix

--- Plot.java
+++ Plot.java
@@ -2,6 +2,7 @@ import javax.swing.JPanel;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.util.Arrays;
 
 
 public class Plot extends JPanel {
@@ -25,42 +26,61 @@ public class Plot extends JPanel {
         super.paintComponent(g);
 
         int panelWidth = getWidth();
-        int panelHeight = getHeight() - 20;
+        int panelHeight = getHeight();
+        int margin = 20;
 
-        int numberOfLines = data[0].length;
-        int lineSpacing = panelWidth / (numberOfLines + 1);
+        int axisCount = data[0].length;
+        int lineSpacing = panelWidth / (axisCount + 1);
 
         g.setColor(Color.BLACK);
 
-        float[] maxes = new float[numberOfLines - 1];
+        float[] maxes = new float[axisCount - 1]; // init to all 0s
+        float[] mins = new float[axisCount - 1]; // init to all max values
+        Arrays.fill(mins, Float.MAX_VALUE);
+
         for (int j = 1; j < data.length; j++) {
-            for (int i = 0; i < numberOfLines - 1; i++) {
+            for (int i = 0; i < axisCount - 1; i++) {
                 Float dataPnt = Float.parseFloat(data[j][i]);
                 if (dataPnt > maxes[i]) {
                     maxes[i] = dataPnt;
                 }
+                if (dataPnt < mins[i]) {
+                    mins[i] = dataPnt;
+                }
             }
         }
+
+        System.out.println(Arrays.toString(mins));
         
-        for (int i = 1; i <= numberOfLines; i++) {
+        for (int i = 1; i <= axisCount; i++) {
             int x = lineSpacing * i;
-            g.drawLine(x, 20, x, panelHeight); // axis
+            g.drawLine(x, 20, x, panelHeight - margin); // axis
         }
 
         for (int j = 1; j < data.length; j++) {
-            if (j == numberOfLines) continue;
-            for (int i = 0; i < numberOfLines - 1; i++) {
-                int x = lineSpacing * (i + 1);
-                Float dataPnt = Float.parseFloat(data[j][i]);
+             // Generate random RGB values
+            int red = (int) (Math.random() * 256);
+            int green = (int) (Math.random() * 256);
+            int blue = (int) (Math.random() * 256);
 
-                int pos = Math.round(dataPnt * ((panelHeight - 40)) / maxes[i]);
+            // Create a random color
+            Color randomColor = new Color(red, green, blue);
 
-                g.drawOval(x - 3, panelHeight - pos + 20 - 3, 6, 6);
+            // Set the color of the graphics object
+            g.setColor(randomColor);
+
+            for (int i = 1; i < axisCount; i++) {
+                int x = lineSpacing * i;
+                Float dataPnt = Float.parseFloat(data[j][i-1]);
+
+                int pos = Math.round((panelHeight - margin - margin) * ((dataPnt - mins[i-1]) / (maxes[i-1] - mins[i-1])) + margin);
+
+                g.drawOval(x - 3, panelHeight - pos - 3, 6, 6);
                 
-                if (i < numberOfLines - 2) {
-                    Float nextDataPnt = Float.parseFloat(data[j][i + 1]);
-                    int nextPos = Math.round(nextDataPnt * ((panelHeight - 40)) / maxes[i + 1]);
-                    g.drawLine(x, panelHeight - pos + 20, lineSpacing * (i + 2), panelHeight - nextPos + 20);
+                if (i < axisCount - 1) {
+                    Float nextDataPnt = Float.parseFloat(data[j][i]);
+                    int nextPos = Math.round((panelHeight - margin - margin) * ((nextDataPnt - mins[i]) / (maxes[i] - mins[i])) + margin);
+                    g.drawLine(x, panelHeight - pos, lineSpacing * (i + 1), panelHeight - nextPos);
                 }
             }
         }
--- README.md
+++ README.md
@@ -12,5 +12,6 @@ Parses CSV files (must have class/id in the lass column for now) and displays in
 
 ## Early screenshots
 
+![Iris dataset with random colors](screenshots/iris_with_random_colors.png)
 ![first wbc diag render](screenshots/first_wbc_diag_render.png)
 ![first mnist letters render](screenshots/first_mnist_letters_render.png)
